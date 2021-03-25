a80e2917a18ab949ff2b38f80b205e850698c448
Ben Larralde <728046+blarralde@users.noreply.github.com>
2021-03-25 11:09:45 -0700

    first commit

---
+++ api/data/Post.ts
@@ -0,0 +1,25 @@
+export default {
+  attributes: {
+    title: {
+      type: 'string',
+      validation: {
+        required: true
+      }
+    },
+    content: {
+      type: 'string'
+    },
+    published: {
+      type: 'boolean',
+      default: false
+    },
+    viewCount: {
+      type: 'integer',
+      default: 0
+    },
+    author: {
+      type: 'relation',
+      relation: 'User'
+    }
+  }
+};
---
+++ api/data/User.ts
@@ -0,0 +1,19 @@
+export default {
+  attributes: {
+    email: {
+      type: 'string',
+      unique: true,
+      validation: {
+        required: true
+      }
+    },
+    name: {
+      type: 'string'
+    },
+    posts: {
+      type: 'relation',
+      relation: 'Post[]'
+    },
+    skipTimestamps: true
+  }
+};
